6a957b1839063992314beb39a2ddd1dd8fd04b3d
TWINKLE006-STAR <twinklemakhija57@gmail.com>
2026-07-22 19:52:49 +0530

    change

--- localrepo/p.java
+++ localrepo/p.java
@@ -1,18 +1,19 @@
 import java.util.*;
 class p{
+   //one way
 public void pattern(int n){
     //outer
 for(int i=1;i<=n;i++){
-    //inner
+    
     for(int j=1;j<=n;j++){
-     if(j<=i){
-        System.out.print("*");
-
-     }   
-     else{
-        System.out.print("_");
-
-     }
+   if(i-j>=0){
+    System.out.print("*");
+                
+}
+ else{
+                    
+ System.out.print("_");
+                }
     }
     System.out.println();
 }
